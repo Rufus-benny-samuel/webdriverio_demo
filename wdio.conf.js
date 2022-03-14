97022aa9c4e6ded41b86d87f9080af0109cadef2
Rufus Reishma <rufusreishma@Rufuss-MacBook-Air.local>
2022-03-14 18:42:40 +0530

    chromdriver change

--- wdio.conf.js
+++ wdio.conf.js
@@ -193,15 +193,15 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-   'services': ['selenium-standalone'],
-  //  services: [
-  //   ['chromedriver', {
-  //       logFileName: 'wdio-chromedriver.log', // default
-  //       outputDir: 'driver-logs', // overwrites the config.outputDir
-  //       args: ['--silent'],
-  //       chromedriverCustomPath: '/Users/rufusreishma/Downloads/chromedriver'
-  //   }]
-  // ],
+ //  'services': ['selenium-standalone'],
+   services: [
+    ['chromedriver', {
+        logFileName: 'wdio-chromedriver.log', // default
+        outputDir: 'driver-logs', // overwrites the config.outputDir
+        args: ['--silent'],
+  //       chromedriverCustomPath: '/Users/rufus/Downloads/chromedriver'
+    }]
+  ],
 
   // services:[ ['chromedriver', {
   //     //logPath: 'logs',
